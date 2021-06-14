c0c1a6dff13e1e3b0c7a74e9042446b71337cc43
Rúben Freitas <joseruben98@hotmail.com>
2021-06-14 11:18:38 +0100

    new weka+api project for verison 9.1.0.324

---
+++ PBI/PBI2/Current/implementacao/SAD2021_API+Weka/src/WekaSolution/Transformation.java
@@ -0,0 +1,48 @@
+package WekaSolution;
+
+import java.io.File;
+import java.util.ArrayList;
+import org.pentaho.di.core.KettleEnvironment;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.logging.LogLevel;
+import org.pentaho.di.trans.Trans;
+import org.pentaho.di.trans.TransMeta;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ruben
+ */
+public class Transformation {
+
+    static void RunTransformations() throws KettleException {
+        
+        File folder = new File("../");
+        File[] listOfFiles = folder.listFiles();
+        
+        KettleEnvironment.init();
+        
+        for (File listOfFile : listOfFiles) {
+            String fileName = listOfFile.getName();
+            String fileExtension = fileName.substring(fileName.length()-3);
+            if( fileExtension.equals("ktr")){
+
+                TransMeta transMeta = new TransMeta("../" + fileName);
+                Trans trans = new Trans(transMeta);
+                trans.setLogLevel(LogLevel.ERROR);
+                trans.execute(null);
+                trans.waitUntilFinished();
+                
+                if(trans.getErrors() > 0){
+                    System.out.println("Some errors ocurred");
+                }
+            }
+        }     
+    }
+    
+}
