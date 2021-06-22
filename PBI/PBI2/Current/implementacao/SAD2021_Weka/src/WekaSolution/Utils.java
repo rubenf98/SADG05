54b9669d152fceeee970d380a1c07c936a2ddbed
LeonardoTadeuNunesAbreu <38863505+LeonardoTadeuNunesAbreu@users.noreply.github.com>
2021-06-22 20:03:41 +0100

    Clean directory
    Delete all temp file.

---
+++ PBI/PBI2/Current/implementacao/SAD2021_Weka/src/WekaSolution/Utils.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WekaSolution;
+
+import java.io.File;
+import java.util.ArrayList;
+import weka.core.Instances;
+
+/**
+ *
+ * @author tadeu
+ */
+public class Utils {
+    static ArrayList<String> GetCSVFilesList(String path, String extension, int extensionSize) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        
+        ArrayList<String> result = new ArrayList<String>();
+        
+        for(int i = 0; i < listOfFiles.length; i++){
+            String fileName = listOfFiles[i].getName();
+            String fileExtension = fileName.substring(fileName.length()-extensionSize);
+            
+            if( fileExtension.equals(extension)){
+                result.add(fileName);
+            }
+        }
+        
+        return result;
+    }
+    
+    static void cleanDirectoryTempFiles(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+        
+        for(int i = 0; i < listOfFiles.length; i++){
+            String fileName = listOfFiles[i].getName();
+            String fileExtension = fileName.substring(fileName.length()-8);
+            if( fileExtension.equals("_mod.csv")){
+                File file = new File(path + fileName);
+                
+                if(file.delete())
+                {
+                    System.out.println("File deleted: "+file);
+                }
+            }
+        }
+    }
+    
+}
--- PBI/PBI2/Current/implementacao/SAD2021_Weka/src/WekaSolution/WekaApriori.java
+++ PBI/PBI2/Current/implementacao/SAD2021_Weka/src/WekaSolution/WekaApriori.java
@@ -11,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 /**
  *
  * @author tadeu
@@ -21,20 +22,30 @@ public class WekaApriori {
    
     public static void RunAlgorithm() throws FileNotFoundException, IOException, Exception  {
         
-        String filePath = "../test_conv/TASKDATA6.arff";
+        String folderPath = "../";
+        ArrayList<String> arffFilesList = Utils.GetCSVFilesList(folderPath,"arff",4);
         
-	// Carrega dados dos ficheiros
-        Instances data = new Instances(new BufferedReader(new FileReader(filePath)));
-        
-	// Apriori
-	Apriori model = new Apriori();
-	model.buildAssociations(data);
-	System.out.println(model);
+        for (int i = 0; i < arffFilesList.size(); i++){
+            String fileName = arffFilesList.get(i);
+            fileName = fileName.substring(0,fileName.length()-4);
+            
+            String filePath = folderPath + fileName +"arff";
+            
+            // Carrega dados dos ficheiros
+            BufferedReader bf= new BufferedReader(new FileReader(filePath));
+            Instances data = new Instances(bf);
+
+            // Apriori
+            Apriori model = new Apriori();
+            model.buildAssociations(data);
+            System.out.println("---------------------------------------------------------------------------------");
+            System.out.println(fileName);
+            System.out.println(model);
+            System.out.println("---------------------------------------------------------------------------------");
+            data.clear();
+            bf.close();  
+            
+        }
 	
-        /*
-	FPGrowth fpgModel = new FPGrowth();
-	fpgModel.buildAssociations(data);
-	System.out.println(fpgModel);
-        */
     }   
 }
